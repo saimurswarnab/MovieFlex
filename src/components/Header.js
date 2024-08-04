7ee6b3428b0ebdff0f089eaf294cb387286bec60
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-04 22:56:08 +0600

    Search button clickable

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ export const Header = () => {
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MovieFlex</span>
   </Link>
   <div className="flex md:order-2">
-    <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+    <button onClick={()=> setHidden(!hidden)} type="button"  data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
       <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
       </svg>
