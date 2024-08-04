86e62dba41e1c30b1ec61f8b9b850078f767a8be
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-05 03:58:08 +0600

    Bar in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ export const Header = () => {
   const inActiveClass ="text-base block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
   return (
     <header>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <nav className="bg-white border-b-2 border-gray-200 dark:bg-gray-900 dark:border-b-1 dark:border-gray-900">
          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src={Logo} className="size-12 " alt="MovieFlex Logo" />
