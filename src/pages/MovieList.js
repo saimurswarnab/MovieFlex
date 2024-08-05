1b717f911d38c5cf984750adfcb075570457e8a8
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-06 03:10:48 +0600

    MovieList.js

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -5,11 +5,6 @@ export const MovieList = () => {
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap">
           <Card/>
-       
-
-
-
-
         </div>
 
       </section>
