e05542b1b0df0f92eec8327038ba404a6d9c2841
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-07 19:29:06 +0600

    Basic change only get card

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react"
 import { Card } from "../components"
 export const MovieList = () => {
+  const [movies,setMovies]= useState([]);
+
+  useEffect(()=>{
+    async function fetchMovies() {
+      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=00c169398dd18058872947de2d3d3a0e");
+      const data = await response.json();
+      setMovies(data.results);
+      
+    }
+    fetchMovies();
+
+  },[])
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
         <div className="flex justify-start flex-wrap">
-          <Card/>
+          {movies.map((movie)=>(
+            <Card/>
+
+
+          ))}
+          
         </div>
 
       </section>
