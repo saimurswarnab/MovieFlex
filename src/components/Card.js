e1f2d5afe06ba765b97fc9002553b716c049815c
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-07 20:57:05 +0600

    overview

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 export const Card = ({movie}) => {
-    const{id, original_title}=movie;
+    const{id, original_title,overview}=movie;
   return (
     
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
@@ -11,7 +11,7 @@ export const Card = ({movie}) => {
         <Link to="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{original_title}</h5>
         </Link>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
 
     </div>
 </div>
