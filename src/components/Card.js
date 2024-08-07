65fc6c7b1b33b8ff93c995d081c87fb711c6c659
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-07 20:58:49 +0600

    add link

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,11 +4,11 @@ export const Card = ({movie}) => {
   return (
     
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
-    <Link to="#">
+    <Link to={`/movie/${id}`}>
         <img className="rounded-t-lg" src="" alt="" />
     </Link>
     <div className="p-5">
-        <Link to="#">
+        <Link to={`/movie/${id}`}>
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{original_title}</h5>
         </Link>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
