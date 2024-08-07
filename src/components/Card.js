bdc3493acd3334cb2f5cd99ed18454fef7a64f25
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-07 21:19:52 +0600

    for image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 export const Card = ({movie}) => {
-    const{id, original_title,overview}=movie;
+    const{id, original_title,overview,poster_path}=movie;
+    const image = `https://image.tmdb.org/t/p/w500/${poster_path}`;
   return (
     
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
     <Link to={`/movie/${id}`}>
-        <img className="rounded-t-lg" src="" alt="" />
+        <img className="rounded-t-lg" src={image} alt="" />
     </Link>
     <div className="p-5">
         <Link to={`/movie/${id}`}>
