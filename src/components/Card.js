baa3374789aa0b15c77296be98701c9a0e341ebd
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-07 21:24:50 +0600

    if there is no poster path

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
+import Backup from "../assets/images/backup.png"
 export const Card = ({movie}) => {
     const{id, original_title,overview,poster_path}=movie;
-    const image = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+    const image = false ? `https://image.tmdb.org/t/p/w500/${poster_path}` : Backup;
   return (
     
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-3">
