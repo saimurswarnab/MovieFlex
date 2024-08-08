d32cf37814b37431ebc560df051fdc390768c617
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-08 19:27:03 +0600

    apiPath

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import { Card } from "../components"
 import { useFetch } from "../hooks/useFetch";
-export const MovieList = () => {
-  const{data:movies} = useFetch("https://api.themoviedb.org/3/movie/now_playing?api_key=00c169398dd18058872947de2d3d3a0e");
+export const MovieList = ({apiPath}) => {
+  const{data:movies} = useFetch(apiPath);
 
 
   return (
