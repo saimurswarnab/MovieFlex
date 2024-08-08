1ce22838ce342c57279d875f715e9cd1b5e2f702
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-08 19:12:25 +0600

    remove use effect from here and add the useFectch hook

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,18 +1,9 @@
-import { useEffect, useState } from "react"
 import { Card } from "../components"
+import { useFetch } from "../hooks/useFetch";
 export const MovieList = () => {
-  const [movies,setMovies]= useState([]);
+  const{data:movies} = useFetch("https://api.themoviedb.org/3/movie/now_playing?api_key=00c169398dd18058872947de2d3d3a0e");
 
-  useEffect(()=>{
-    async function fetchMovies() {
-      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=00c169398dd18058872947de2d3d3a0e");
-      const data = await response.json();
-      setMovies(data.results);
-      
-    }
-    fetchMovies();
 
-  },[])
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
