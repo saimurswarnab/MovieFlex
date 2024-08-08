a552d22b79c36592000f46e7c9987e66982def71
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-08 19:27:17 +0600

    rename

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -7,11 +7,11 @@ export const AllRoutes = () => {
   return (
     <div className="dark:bg-slate-800">
     <Routes>
-        <Route path="" element={<MovieList api="movie/now_playing"/>}/>
+        <Route path="" element={<MovieList apiPath="movie/now_playing"/>}/>
         <Route path="movie/:id" element={<MovieDetail/>}/>
-        <Route path="movies/popular" element={<MovieList api="movie/popular"/>}/>
-        <Route path="movies/top" element={<MovieList api="movie/top_rated"/>}/>
-        <Route path="movies/upcoming" element={<MovieList api="movie/upcoming"/>}/>
+        <Route path="movies/popular" element={<MovieList apiPath="movie/popular"/>}/>
+        <Route path="movies/top" element={<MovieList apiPath="movie/top_rated"/>}/>
+        <Route path="movies/upcoming" element={<MovieList apiPath="movie/upcoming"/>}/>
         <Route path="search" element={<Search/>}/>
         <Route path="*" element={<PageNotFound/>}/>
     </Routes>
