8490ca06168a2d89beadcec94837fb0e40a9b197
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-08 19:25:50 +0600

    url to apiPath

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,8 @@
 import { useEffect,useState } from "react"
 
-export const useFetch = (url) => {
+export const useFetch = (apiPath) => {
     const [data,setData] =useState([]);
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=00c169398dd18058872947de2d3d3a0e`
     useEffect(()=>{
         async function fetchMovies() {
           const response = await fetch(url);
