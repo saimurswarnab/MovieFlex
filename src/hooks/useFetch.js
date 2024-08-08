7f082568432b7502debbc592e874f5093d3570fb
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-08 19:12:00 +0600

    use the effect here

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import { useEffect,useState } from "react"
 
-export const useFetch = () => {
-  return (
-    <div>useFetch</div>
-  )
+export const useFetch = (url) => {
+    const [data,setData] =useState([]);
+    useEffect(()=>{
+        async function fetchMovies() {
+          const response = await fetch(url);
+          const json = await response.json();
+          setData(json.results);
+          
+        }
+        fetchMovies();
+    
+      },[url])
+  return {data}
 }
 
