6094421335b24961212c63af4520efa5ee66d33e
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-09 18:50:21 +0600

    js

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,4 @@
-import { Card } from "../components"
+import { Card } from "../components";
 import { useFetch } from "../hooks/useFetch";
 export const MovieList = ({apiPath}) => {
   const{data:movies} = useFetch(apiPath);
