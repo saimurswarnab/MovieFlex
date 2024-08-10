09e322341a8c3ddf241b4565225a8ecf0c035854
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-10 19:56:56 +0600

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,7 +33,7 @@ export const Header = () => {
   
   return (
     <header>
-      <nav className="bg-white border-b-2 border-gray-200 dark:bg-gray-900 dark:border-b-1 dark:border-gray-900">
+      <nav className="bg-white border-b-2 border-gray-600 dark:bg-darkbg dark:border-b-1 dark:bg-darkbg">
          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src={Logo} className="size-12 mr-2 h-6 sm:h-9" alt="MovieFlex Logo" />
@@ -86,7 +86,7 @@ export const Header = () => {
                   </form>
                   
                 </div>
-                <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-darkbg md:dark:bg-darkbg dark:border-gray-700">
                   <li>
                     <NavLink to="/" className={({isActive})=> isActive ? activeClass : inActiveClass} end>Home</NavLink>
                   </li>
