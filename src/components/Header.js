676834cc88d50929a51e4441506477c839582d92
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-10 19:24:59 +0600

    css change

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ export const Header = () => {
       <nav className="bg-white border-b-2 border-gray-200 dark:bg-gray-900 dark:border-b-1 dark:border-gray-900">
          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src={Logo} className="size-12 " alt="MovieFlex Logo" />
+                <img src={Logo} className="size-12 mr-2 h-6 sm:h-9" alt="MovieFlex Logo" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">MovieFlex</span>
             </Link>
 
