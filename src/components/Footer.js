9bc5b8d04c6f39d47adf00bab89621a14362467e
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-10 19:56:49 +0600

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 export const Footer = () => {
   return (
-      <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+      <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-darkbg">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="/" className="hover:underline">MovieFlex</Link>. All Rights Reserved. </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
               <li>
