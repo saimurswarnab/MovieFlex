6ee552230192196d504aff346c61397fe57624f6
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-10 19:56:40 +0600

    change color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
       extend: {
         screens:{
           "other":{'min': '340px', 'max': '1200px'}
+        },
+        colors:{
+          darkbg:"#232323",
         }
       },
 
