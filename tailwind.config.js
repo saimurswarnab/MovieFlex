9d0e7ffe8fd4cd1f62488679e72c2cba41271d58
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-10 19:43:04 +0600

    custom screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     darkMode: 'selector',
     theme: {
-      extend: {},
+      extend: {
+        screens:{
+          "other":{'min': '340px', 'max': '1200px'}
+        }
+      },
 
     },
     plugins: [],
