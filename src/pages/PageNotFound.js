fa7fa552bc6217491d306a7f63742dd0bf74c642
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-12 19:11:54 +0600

    css

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,7 +1,15 @@
 export const PageNotFound = () => {
   return (
     <main>
-      
+      <section className="flex flex-col justify-center px-2">
+        <div className="flex flex-col items-center my-4">
+
+        </div>
+
+        <div>
+
+        </div>
+      </section>
     </main>
   )
 }
