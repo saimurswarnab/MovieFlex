126cff4efe60364b4289aa5d5f4e9f423f7b3c29
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-12 19:30:41 +0600

    button

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import PageNotFoundImage from "../assets/images/pagenotfound.png"
 export const PageNotFound = () => {
   return (
@@ -5,11 +6,16 @@ export const PageNotFound = () => {
       <section className="flex flex-col justify-center px-2">
         <div className="flex flex-col items-center my-4">
           <p className="text-7xl text-black font-bold my-10 dark:text-white">404, Opps!!</p>
-          <img className="rounded" src={PageNotFoundImage} alt="404 Page Not Found"/>
+          <div className="max-w-lg">
+           <img className="rounded" src={PageNotFoundImage} alt="404 Page Not Found"/>
+          </div>
 
         </div>
 
-        <div>
+        <div className="flex justify-center my-4">
+          <Link to="/">
+           <button className="w-64 text-xl text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 rounded-lg px-5 py-2.5 mr-2 mb-2 font-medium"> Back To MovieFlex</button>
+          </Link>
 
         </div>
       </section>
