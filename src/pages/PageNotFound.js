fa2f64a3000ba089d610d8299b1f3b0d6b2d132b
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-12 19:33:58 +0600

    js

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -16,7 +16,6 @@ export const PageNotFound = () => {
           <Link to="/">
            <button className="w-64 text-xl text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 rounded-lg px-5 py-2.5 mr-2 mb-2 font-medium"> Back To MovieFlex</button>
           </Link>
-
         </div>
       </section>
     </main>
