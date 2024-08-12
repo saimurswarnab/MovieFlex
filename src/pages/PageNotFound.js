70aaadf12cf30f78486a367b27769babe5f5143b
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-12 19:36:09 +0600

    js

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -14,7 +14,7 @@ export const PageNotFound = () => {
 
         <div className="flex justify-center my-4">
           <Link to="/">
-           <button className="w-64 text-xl text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 rounded-lg px-5 py-2.5 mr-2 mb-2 font-medium"> Back To MovieFlex</button>
+           
           </Link>
         </div>
       </section>
