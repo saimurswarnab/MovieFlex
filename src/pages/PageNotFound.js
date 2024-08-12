359f0ddf12cd87e258e4950d53bf6ba616e1c033
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-12 19:16:19 +0600

    image and 404 opps text

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,8 +1,11 @@
+import PageNotFoundImage from "../assets/images/pagenotfound.png"
 export const PageNotFound = () => {
   return (
     <main>
       <section className="flex flex-col justify-center px-2">
         <div className="flex flex-col items-center my-4">
+          <p className="text-7xl text-black font-bold my-10 dark:text-white">404, Opps!!</p>
+          <img className="rounded" src={PageNotFoundImage} alt="404 Page Not Found"/>
 
         </div>
 
