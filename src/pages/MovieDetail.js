173e77cf117d2686ba0202ab57bc385095e53ed8
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-15 19:38:46 +0600

    for poster image

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,15 +1,17 @@
 import { useParams } from "react-router-dom";
 import { useState,useEffect } from "react";
+import Backup from"../assets/images/backup.png"
 
 export const MovieDetail = () => {
   const params = useParams();
-  const[data,setData] = useState({});
+  const[movie,setMovie] = useState({});
+  const image = movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : Backup;
 
   useEffect(()=>{
     async function fetchMovie(){
       const response = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=00c169398dd18058872947de2d3d3a0e`);
       const json = await response.json();
-      setData(json);
+      setMovie(json);
       
     }
     fetchMovie();
@@ -17,8 +19,11 @@ export const MovieDetail = () => {
   },[])
   return (
     <main>
-      <section>
-        <div></div>
+      <section className="flex justify-around flex-wrap py-5">
+        <div className="max-w-sm">
+          <img className="rounded" src={image} alt={movie.title}/>
+
+        </div>
         <div></div>
       </section>
     </main>
