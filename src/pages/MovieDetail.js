2ff1c08c8660116d80e115f6901b9d4df15e514c
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-15 19:06:42 +0600

    First stage get information

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,4 +1,20 @@
+import { useParams } from "react-router-dom";
+import { useState,useEffect } from "react";
+
 export const MovieDetail = () => {
+  const params = useParams();
+  const[data,setData] = useState({});
+
+  useEffect(()=>{
+    async function fetchMovie(){
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=00c169398dd18058872947de2d3d3a0e`);
+      const json = await response.json();
+      setData(json);
+      
+    }
+    fetchMovie();
+
+  },[])
   return (
     <main>
       <section>
