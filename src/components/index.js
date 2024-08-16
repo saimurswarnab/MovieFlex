e5b7ff321e509187c0b6ad05c0fe243f05ed456f
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-16 19:39:19 +0600

    js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,10 @@
 export {Header} from './Header';
+
 export {Footer} from './Footer';
 export {Card} from './Card';
 
 export {Button} from './Button';
+
+export { ScrollToTop } from './ScrollToTop';
 
 
