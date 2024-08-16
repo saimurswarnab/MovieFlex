6f8eed6b61c59420b85f116b5ccc0534770836f9
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-16 19:23:55 +0600

    Add the file

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
+import ScrollToTop from './components/ScrollToTop';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop/>
       <App />
     </Router>
   </React.StrictMode>
