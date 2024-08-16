58b5429340994698841559935fcb1877bcf99d9b
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-16 13:09:42 +0600

    Js

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -6,10 +6,9 @@ export const PageNotFound = () => {
     <main>
       <section className="flex flex-col justify-center px-2">
         <div className="flex flex-col items-center my-4">
-          <p className="text-7xl text-black font-bold my-10 dark:text-white">404, Opps!!</p>
-          <div className="max-w-lg">
-           <img className="rounded" src={PageNotFoundImage} alt="404 Page Not Found"/>
-          </div>
+          <h1 class="text-9xl font-black text-gray-200 dark:text-gray-700">404</h1>
+          <p class="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-white">Uh-oh!</p>
+          <p class="mt-4 text-gray-500 dark:text-gray-400">We can't find that page.</p>
 
         </div>
 
