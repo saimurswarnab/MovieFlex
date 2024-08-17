fe313574efd561c79469847d3c6adb8f50830777
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-17 19:47:01 +0600

    js

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,7 +14,6 @@ export const Card = ({movie}) => {
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{original_title}</h5>
         </Link>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>
-
     </div>
 </div>
   )
