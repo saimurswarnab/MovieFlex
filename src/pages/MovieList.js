99c2a410477487de5ef50e4365e9bdcfa8b2b425
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-18 18:55:09 +0600

    For title

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import { Card } from "../components";
 import { useFetch } from "../hooks/useFetch";
-export const MovieList = ({apiPath}) => {
+export const MovieList = ({apiPath,title}) => {
   const{data:movies} = useFetch(apiPath);
+
+  useEffect(()=>{
+    document.title = `${title}/ Movieflex`;
+
+  });
 
 
   return (
