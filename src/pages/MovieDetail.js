17c257732421d73d238686a7f64b6a97c28721ac
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-18 19:30:07 +0600

    usehook

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,12 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useState,useEffect } from "react";
-import Backup from"../assets/images/backup.png"
+import Backup from"../assets/images/backup.png";
+import { useTitle } from './../hooks/useTitle';
 
 export const MovieDetail = () => {
   const params = useParams();
   const[movie,setMovie] = useState({});
   const image = movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : Backup;
-
+  const pageTitle = useTitle(movie.title);
   useEffect(()=>{
     async function fetchMovie(){
       const response = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=00c169398dd18058872947de2d3d3a0e`);
@@ -18,10 +19,6 @@ export const MovieDetail = () => {
 
   },[]);
 
-  useEffect(()=>{
-    document.title = `${movie.title}/ Movieflex`;
-
-  });
   return (
     <main>
       <section className="flex justify-around flex-wrap py-5">
