d586ac6abddad1f5b04879bb1ebc4fe697b001a7
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-19 21:09:24 +0600

    Page Not Found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom"
-import PageNotFoundImage from "../assets/images/pagenotfound.png"
 import { Button } from "../components/Button"
 import { useEffect } from "react"
 export const PageNotFound = () => {
