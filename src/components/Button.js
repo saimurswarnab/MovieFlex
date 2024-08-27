92e84ff6579618e733ab10bc2e4d7b557bf776d8
MD Saimur Rahman Swarnab <Saimur101@gmail.com>
2024-08-27 19:44:27 +0600

    Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,4 +3,3 @@ export const Button = ({children}) => {
     <button className="w-64 text-xl text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 rounded-lg px-5 py-2.5 mr-2 mb-2 font-medium">{children}</button>
   )
 }
-
